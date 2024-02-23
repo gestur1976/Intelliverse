2a8e79064b8a34b4130d24bd47c139b99ceda3f2
Josep F. Hervás Fuertes <josep.hervas@gmail.com>
2024-02-23 19:17:39 +0100

    [WIP] Added a list of articles from topic at second level.

--- app/Views/topic_articles.php
+++ app/Views/topic_articles.php
@@ -1,12 +1,10 @@
 
-<!-- app/Views/index.php -->
-        <div class="px-4 py-5 my-5 text-center">
-            <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>
-            <div class="col-lg-6 mx-auto">
-                <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                    <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
-                </div>
-            </div>
-        </div>
+<!-- app/Views/topic_articles.php -->
+<div class="container">
+    <h1>Interesting <?= esc($topic) ?> articles:</h1>
+    <ul class="list-group">
+        <?php for ($num = 1; $num <= 10; $num++): ?>
+        <li class="list-group-item"><?php echo ucfirst($topic)?> article <?= esc($num) ?> </li>
+        <?php endfor; ?>
+    </ul>
+</div>
