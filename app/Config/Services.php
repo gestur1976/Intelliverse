ca47eb0093ad1887d682b87392a52fc20035d285
Josep F. Hervás Fuertes <josep.hervas@gmail.com>
2024-02-23 19:15:56 +0100

    [ADD] Added OpenAI service.

--- app/Config/Services.php
+++ app/Config/Services.php
@@ -19,14 +19,11 @@ use CodeIgniter\Config\BaseService;
  */
 class Services extends BaseService
 {
-    /*
-     * public static function example($getShared = true)
-     * {
-     *     if ($getShared) {
-     *         return static::getSharedInstance('example');
-     *     }
-     *
-     *     return new \CodeIgniter\Example();
-     * }
-     */
+    public static function openai($getShared = true)
+    {
+        if ($getShared) {
+            return static::getSharedInstance('openai');
+        }
+        return new \Orhanerday\OpenAi\OpenAi($_ENV['OPENAI_API_KEY']);
+    }
 }
